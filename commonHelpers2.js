4f6450c8c978936912e404424960a0242c9c73d6
EspFerevo <150734998+EspFerevo@users.noreply.github.com>
2024-03-24 12:04:34 +0000

    Deploying to gh-pages from @ EspFerevo/goit-js-hw-09@262c5df7ec05b779db8bd4f54c529ccc8783f7b8 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,6 @@
-import"./assets/styles-0ce8da34.js";document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".feedback-form"),t=JSON.parse(localStorage.getItem("feedback-form-state"))||{};t.email&&(e.elements.email.value=t.email),t.message&&(e.elements.message.value=t.message),e.addEventListener("input",function(a){t[a.target.name]=a.target.value.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(t))}),e.addEventListener("submit",function(a){a.preventDefault();const m=e.elements.email.value.trim(),s=e.elements.message.value.trim();m&&s?(console.log({email:m,message:s}),localStorage.removeItem("feedback-form-state"),e.reset()):alert("!!!!!")})});
+import"./assets/styles-efcbad89.js";//!
+document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".feedback-form"),t=JSON.parse(localStorage.getItem("feedback-form-state"))||{};//!
+t.email&&(e.elements.email.value=t.email),t.message&&(e.elements.message.value=t.message);//!
+e.addEventListener("input",function(a){t[a.target.name]=a.target.value.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(t))});//!
+e.addEventListener("submit",function(a){a.preventDefault();const m=e.elements.email.value.trim(),s=e.elements.message.value.trim();m&&s?(console.log({email:m,message:s}),localStorage.removeItem("feedback-form-state"),e.reset()):alert("!!!!!")})});
 //# sourceMappingURL=commonHelpers2.js.map
