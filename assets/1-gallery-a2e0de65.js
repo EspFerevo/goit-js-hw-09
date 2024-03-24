c2d053bc5add9ae8c1cbab4082ef7528dcf31a48
EspFerevo <150734998+EspFerevo@users.noreply.github.com>
2024-03-24 10:57:27 +0000

    Deploying to gh-pages from @ EspFerevo/goit-js-hw-09@60bca0a493a4be02ee6b58b9cde440cb0d98c4b0 🚀

--- assets/1-gallery-a2e0de65.js
+++
@@ -1,2 +0,0 @@
-import{S as a}from"./vendor-870f0eb5.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=1-gallery-a2e0de65.js.map
---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-0ce8da34.js";import{S as a}from"./assets/vendor-870f0eb5.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=commonHelpers.js.map
